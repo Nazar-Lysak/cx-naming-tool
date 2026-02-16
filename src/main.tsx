4aed9cc59f8311b3fc1d927378564cf68a18904a
Nazarii Lysak <nazarii.lysak@ua.nestle.com>
2026-02-16 18:36:19 +0200

    add cdn script data

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-// Експортуємо App для CDN/UMD використання
 export default App;
+
+const cdn = document.currentScript as HTMLScriptElement;
+const dcnConfig = { 
+  id: cdn?.getAttribute('data-domain-script'), 
+  lang: cdn?.getAttribute('data-language') 
+};
+
+console.log('CX Naming Tool Widget Config:', dcnConfig);
 
 // Автоматично рендеримо для SPA (standalone/demo)
 const rootElement = document.getElementById('cx-naming-tool-widget');
